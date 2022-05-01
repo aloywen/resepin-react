cef7dc85bb4e9714459401d374173f926274861d
aloywen <aliianwar7@gmail.com>
2022-05-01 22:49:34 +0700

    first commit

---
+++ src/container/pages/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <button className='bg-yellow-400 rounded-lg px-20 py-2 text-2xl mt-2 font-semibold text-white hover:bg-yellow-500 shadow-lg'>Let's go guys!</button>
+    </div>
+  );
+}
+
+export default App;
