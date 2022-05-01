c8bd46fca0e3c7b1dba5bd1ff0c13d94fc83e100
aloywen <aliianwar7@gmail.com>
2022-05-01 23:02:08 +0700

    fix folder

--- src/container/pages/App.js
+++ src/container/pages/App.js
@@ -1,8 +1,9 @@
+import Button from "../../component/atoms/Button";
 
 function App() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <button className='bg-yellow-400 rounded-lg px-20 py-2 text-2xl mt-2 font-semibold text-white hover:bg-yellow-500 shadow-lg'>Let's go guys!</button>
+      <Button />
     </div>
   );
 }
